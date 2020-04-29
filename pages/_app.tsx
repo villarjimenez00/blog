1d1b0341d7ef9fd38b1e3ba88c87b4a46ee70921
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-04-29 20:26:26 +0200

    init proyect

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/index.css";
+
+export default function App({
+  Component,
+  pageProps,
+}: {
+  Component: any;
+  pageProps: any;
+}) {
+  return <Component {...pageProps} />;
+}
